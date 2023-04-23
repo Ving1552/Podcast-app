e97ca8a77d65fdf13db5339125189444ba81a054
gvineeth1552 <gvineeth1552@gmail.com>
2023-04-23 10:27:24 +0530

    useAuth Hook

--- src/server.js
+++ src/server.js
@@ -1,27 +1,33 @@
 //server.js
 const express = require("express");
 const SpotifyWebApi = require("spotify-web-api-node");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(cors())
+app.use(bodyParser.json());
 
 app.post("/login", (req, res) => {
-    const code = req.body.code
-    const spotifyApi = new SpotifyWebApi({
-      redirectUri: 'http://localhost:3000',
-      clientId: '527cba8010a3440ab8bef6bda1dc558a',
-      clientSecret: 'c1d404a4e8564a778481f05e04cfb593',
-    })
-  
-    spotifyApi
-      .authorizationCodeGrant(code)
-      .then(data => {
-        res.json({
-          accessToken: data.body.access_token,
-          refreshToken: data.body.refresh_token,
-          expiresIn: data.body.expires_in,
-        })
-      })
-      .catch(err => {
-        res.sendStatus(400)
-      })
+  const code = req.body.code
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: 'http://localhost:3000',
+    clientId: '527cba8010a3440ab8bef6bda1dc558a',
+    clientSecret: 'c1d404a4e8564a778481f05e04cfb593',
   })
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then(data => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.sendStatus(400)
+    })
+})
+app.listen(3001)
